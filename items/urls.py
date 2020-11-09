6867fe988faab99711207edb18bdbe917ee5e761
rlaehddus <dongyune0409@naver.com>
2020-11-09 21:12:20 +0900

    dislike

--- items/urls.py
+++ items/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create/', views.create, name="create"),
     path('show/<int:post_id>/', views.show, name="show"),
     path('delete/<int:post_id>/', views.delete, name="delete"),
+    path('like_toggle/<int:post_id>/', views.like_toggle, name="like_toggle"),
+    path('dislike_toggle/<int:post_id>/', views.dislike_toggle, name="dislike_toggle"),
 ]
--- items/views.py
+++ items/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from .models import Post,Comment,Like
+from .models import *
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+import json
+
 
 def main(request):
     items = Post.objects.all()
@@ -30,3 +34,43 @@ def delete(request,post_id):
     post = get_object_or_404(Post, pk=post_id)
     post.delete()
     return redirect('main')
+
+@require_POST
+@login_required
+def like_toggle(request, post_id):
+
+    post = get_object_or_404(Post, pk=post_id)
+    post_like, post_like_created = Like.objects.get_or_create(user=request.user, post=post)
+
+    if not post_like_created:
+        post_like.delete()
+        result = "like_cancel"
+    else:
+        result = "like"
+
+    context = {
+        "like_count": post.like_count,
+        "result": result
+    }
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
+
+@require_POST
+@login_required
+def dislike_toggle(request, post_id):
+
+    post = get_object_or_404(Post, pk=post_id)
+    post_dislike, post_dislike_created = Disike.objects.get_or_create(user=request.user, post=post)
+
+    if not post_dislike_created:
+        post_dislike.delete()
+        result = "dislike_cancel"
+    else:
+        result = "dislike"
+
+    context = {
+        "dislike_count": post.like_count,
+        "result": result
+    }
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
